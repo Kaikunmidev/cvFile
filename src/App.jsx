b716ce264e483afb848e6eb84b4e899d32b2d604
Kaikunmidev <110641848+Kaikunmidev@users.noreply.github.com>
2022-12-06 11:44:45 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='Container'>
+      <div className="header"><h1>CURRICULUM VITAE</h1></div>
+      {/* MAINBODY STARTS HERE */}
+      <div className="mainbody">
+      <div className="personalInfo">
+        <h4 className='name'>Full name</h4>
+        <p className="address">Address</p>
+        <h4 className='mobile'>Mobile No:</h4>
+        {/* <p className=''></p> */}
+        <hr />
+        <hr className='lineee' />
+      </div>
+      <section className='Career_part'>
+        <div className="careerObj">
+          <h3 className='careerHeader'>CAREER OBJECTIVE</h3>
+          <p className='career_smallText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, beatae quam nemo obcaecati suscipit quaerat numquam possimus soluta sint modi unde deserunt facere distinctio porro, neque temporibus aliquam id delectus dicta illum? Dignissimos veritatis ea voluptatem ad eveniet saepe, quas voluptas neque unde, harum nam corporis, maiores aspernatur ipsum architecto.</p>
+        </div>
+        <div className="academicQual">
+          <h3 className='academicHeader'>ACADEMIC QUALIFICATION</h3>
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, cum</p>
+          </div>
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, cum</p>
+          </div>
+        </div>
+        <div className="experience">
+          <h3 className="expHeader">EXPERIENCE</h3>
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, cum</p>
+          </div>
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, cum</p>
+          </div>
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, cum</p>
+          </div>
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, cum</p>
+          </div>
+        </div>
+        <div className="personalSkill">
+          <h3 className='personalHeader'>PERSONAL SKILL</h3>
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur</p>
+          </div>
+          <div className="requirment">
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur</p>
+          </div>
+          </div>
+          
+          <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+          <p className="acc">Lorem ipsum dolor sit amet consectetur</p>
+          </div>
+        </div>
+        <div className="personalProfile">
+        <h3 className='personalHeader'>PERSONAL PROFILE</h3>
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+              <h4 className="acc">Father's Name</h4>
+            <p className="ans">Lorem ipsum dolor sit amet </p>
+            </div>
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+              <h4 className="acc">Date Of Birth</h4>
+            <p className="ans2">Lorem ipsum dolor.</p>
+            </div>
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+              <h4 className="acc">Sex</h4>
+            <p className="ans3">Lorem </p>
+            </div>
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+              <h4 className="acc">Marital Status</h4>
+            <p className="ans4">Lorem ipsum</p>
+            </div>
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+              <h4 id='lang' className="acc">Languages  Known</h4>
+            <p className="ans5">English and spanish</p>
+            </div>
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22l-4-4h8l-4 4zm0-20l4 4H8l4-4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM2 12l4-4v8l-4-4zm20 0l-4 4V8l4 4z"/></svg>
+              <h4 className="acc">Nationality</h4>
+            <p className="ans6">Lorem ipsum </p>
+            </div>
+        </div>
+        <div className="declaration">
+        <h3 className='personalHeader'>DECLARATION</h3>
+        <p className="declarationText">I solemnly declare that all the above information is correct to the best of my knowledge abd belief.</p>
+        <span className="date">Date:</span>
+        <div className="place2">
+        <span className="place">Place:</span>
+        <h3 className='nname'>(Your Full Name)</h3>
+        </div>
+        </div>
+      </section>
+      </div>
+    </div>
+  )
+}
+
+export default App
